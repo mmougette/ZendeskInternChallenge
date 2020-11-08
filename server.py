c59a8da7373da00a738c5c6372e36cab9ca5acc4
mmougette <70075617+mmougette@users.noreply.github.com>
2020-11-08 16:50:40 -0600

    Added Sessions and Next/Previous Buttons
    Added sessions and next/previous buttons to page through tickets in chunks of 25. Still no css as of right now - that will come later.

--- server.py
+++ server.py
@@ -4,34 +4,110 @@ Contact:    mougette@wisc.edu
 This project was made for the Zendesk Intern Coding Challenge.
 '''
 
-from flask import Flask, render_template
 import requests
+from flask import Flask, render_template, request, session
+
 
 app = Flask(__name__)
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.secret_key = "Max Is Awesome"
+
+
 @app.route('/')
 def home():
+    ## Secure cookies to store repeatitly used vars
+    session['pageNum'] = 1
+    session['subdomain'] = 'mougette'
+    session['user'] = 'mougette@wisc.edu'
+    session['pswd'] = '***********' ## Temp password for GitHub
+
+    ## Call the Zendesk API for tickets and count.json
+    response = getResponse()
+    jsonResponse = response.json()
+    countResponse = getCountResponse()
+
+    countJsonResponse = countResponse.json()
+    statusCode = response.status_code
+    tickets = jsonResponse['tickets']
+    numTickets = countJsonResponse['count']['value']
+
+    ## more sessions that can now be made after the response
+    session['statusCode'] = response.status_code
+    session['count'] = numTickets
+
+    return render_template('page.html', statusCode = session['statusCode'], tickets = tickets,
+                           numTickets = session['count'], pageNum = session['pageNum'])
+
+@app.route('/<int:page_id>',  methods=['GET', 'POST'])
+def pages(page_id):
+    if request.method == 'POST':
+
+        if (int(page_id) > session['pageNum']):
+            response = getResponse()
+            jsonResponse = response.json()
+            i = 1
+            while (i < int(page_id)):
+
+                jsonResponse = getNextResponse(jsonResponse)
+                statusCode = session['statusCode']
+                i += 1
+
+        else:
+            response = getResponse()
+            jsonResponse = response.json()
+            i = 1
+            while (i < int(page_id)):
+                jsonResponse = getNextResponse(jsonResponse)
+                statusCode = session['statusCode']
+                i += 1
+
+        session['pageNum'] = page_id
+        tickets = jsonResponse['tickets']
+
+        return render_template('page.html', statusCode=session['statusCode'], tickets=tickets,
+                               numTickets=session['count'], pageNum=session['pageNum'])
+
+    return "Whoopsie!!"
+
+
+def getResponse():
     ## Set up the data needed for the Zendesk API request
-    subdomain = 'mougette'
-    user = 'mougette@wisc.edu'
-    pswd = '***********' ## place holder password for github haha
+    subdomain = session['subdomain']
+    user = session['user']
+    pswd = session['pswd']
     url = 'https://' + subdomain + '.zendesk.com/api/v2/tickets.json?page[size]=25'
 
     ## get the response from the Zendesk API
     response = requests.get(url, auth=(user, pswd))
 
-    ## vars to be passed into the render template
-    jsonResponse = response.json()
-    statusCode = response.status_code
-    tickets = jsonResponse['tickets']
+    return response
 
+def getCountResponse():
+    ## Set up the data needed for the Zendesk API request
+    subdomain = session['subdomain']
+    user = session['user']
+    pswd = session['pswd']
+    countURL = 'https://' + subdomain + '.zendesk.com/api/v2/tickets/count.json'
 
-    return render_template('page.html', statusCode = statusCode, tickets = tickets)
+    ## get the response from the Zendesk API
+    countResponse = requests.get(countURL, auth=(user, pswd))
 
-@app.route('/<page_id>')
-def pages(page_id):
-    return 'This is page ' + str(page_id)
+    return countResponse
 
+def getNextResponse(jsonResponse):
+    subdomain = session['subdomain']
+    user = session['user']
+    pswd = session['pswd']
+    hasMore = jsonResponse['meta']['has_more']
+    if (hasMore):
+        nextUrl = jsonResponse['links']['next']
+        response = requests.get(nextUrl, auth=(user, pswd))
+        nextJsonResponse = response.json()
+        session['statusCode'] = response.status_code
+        return nextJsonResponse
+    return None
 
 if __name__ == '__main__':
     app.run(host='localhost', port=3000)
